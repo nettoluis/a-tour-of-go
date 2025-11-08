ff72c8b7d475be92acfc10eecf58b85d4c07b131
nettoluis <luisnetto.2013@gmail.com>
2025-11-08 09:28:12 -0300

    primeira parte do A Tour Of Go

---
+++ exported-names.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+    "fmt"
+    "math"
+)
+
+func main() {
+    fmt.Println(math.Pi);
+    //fmt.Println(math.pi); Esse dá erro porque nomes com início minúsculo não são exportáveis
+}
---
+++ imports.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+    "fmt"
+    "math/rand"
+)
+
+func main() {
+    fmt.Println("O meu número favorito é ", rand.Intn(10))
+}
